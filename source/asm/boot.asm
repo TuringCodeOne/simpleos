9b979397720cfe9e5f9375316c004c2c0fb34840
turingcodeone <turingcodepjw@outlook.com>
2019-10-03 00:31:26 +0800

    feat: add kernel loader
    Signed-off-by: turingcodeone <turingcodepjw@outlook.com>

--- source/asm/boot.asm
+++ source/asm/boot.asm
@@ -1,27 +1,26 @@
-org  0x7c00
-jmp  entry
+org  0x7c00;
+
+LOAD_ADDR  EQU  0X8000
 
 entry:
     mov  ax, 0
     mov  ss, ax
     mov  ds, ax
     mov  es, ax
-    mov  si, msg
+    mov  si, ax
 
-putloop:
-    mov  al, [si]
-    add  si, 1
-    cmp  al, 0
-    je   fin
-    mov  ah, 0x0e
-    mov  bx, 15
-    int  0x10
-    jmp  putloop
+readFloppy:
+    mov          CH, 1         ; CH 用来存储柱面号
+    mov          DH, 0         ; DH 用来存储磁头号
+    mov          CL, 2         ; CL 用来存储扇区号
+    mov          BX, LOAD_ADDR ; ES:BX 数据存储缓冲区
+    mov          AH, 0x02      ; AH = 02 表示要做的是读盘操作
+    mov          AL,  1        ; AL 表示要练习读取几个扇区
+    mov          DL, 0         ; 驱动器编号，一般我们只有一个软盘驱动器，所以写死   
+    INT          0x13          ; 调用BIOS中断实现磁盘读取功能
+    JC           fin
+    jmp          LOAD_ADDR
 
 fin:
     HLT
     jmp  fin
-
-msg:
-    db    "hello, world"
-    db    0
